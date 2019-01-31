28907aa5d688025337f33760e29778ee3444dd43
Sashwat K <sashwat0001@gmail.com>
2019-01-31 22:31:32 +0530

    Added new code

---
+++ cycle-1/7-wc-cut-paste/7-cut.sh
@@ -0,0 +1,2 @@
+# display character set
+cut -b 1,2,3,6 test
---
+++ cycle-1/7-wc-cut-paste/7-wc-cut-paste.sh
@@ -0,0 +1,3 @@
+touch test
+echo "Fedora Red Hat CentOS Fedora Debian OpenSuse Ubuntu Slackware Mandriva" >> test
+wc test # outputs number-of-lines word-count number-of-bytes
