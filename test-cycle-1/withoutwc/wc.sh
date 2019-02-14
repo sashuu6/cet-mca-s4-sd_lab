6eef30321144c9dabaef474dd9391a902bd45703
Sashwat K <sashwat0001@gmail.com>
2019-02-14 12:49:44 +0530

    some message

--- test-cycle-1/withoutwc/wc.sh
+++ test-cycle-1/withoutwc/wc.sh
@@ -1,16 +1,31 @@
-line_count=0
-character_count=0
-alphabet_count=0
+line_count=0;
+character_count=0;
+capital_alphabet_count=0;
+small_alphabet_count=0;
+word_count=0;
+digits_count=0;
 while read p; do
   for (( i=0; i<${#p}; i++ )); do
-	  if [[ "${p:$i:1}" = [a-z] ]]
+	  if [[ "${p:$i:1}" =~ [A-Z] ]]
 	  then
-		alphabet_count=$((alphabet_count+1))
+		capital_alphabet_count=$((capital_alphabet_count+1));
 	  fi
-	   character_count=$((character_count+1))
+	  if [[ "${p:$i:1}" =~ [a-z] ]]
+	  then
+		small_alphabet_count=$((small_alphabet_count+1));
+	  fi
+	  if [[ "${p:$i:1}" = [0-9] ]]
+	  then
+	  	digits_count=$((digits_count+1));
+	  fi
+	  character_count=$((character_count+1));
   done
- line_count=$((line_count+1))
+ word_count=$((word_count+${#p[@]}));
+ line_count=$((line_count+1));
 done < $1
-echo "line count $line_count"
-echo "character count $character_count"
-echo "alphabet count $alphabet_count"
+echo "line count $line_count";
+echo "character count $character_count";
+echo "Capital alphabet count $capital_alphabet_count";
+echo "Small alphabet count $small_alphabet_count";
+echo "Digits count $digits_count";
+echo "word count $word_count";
