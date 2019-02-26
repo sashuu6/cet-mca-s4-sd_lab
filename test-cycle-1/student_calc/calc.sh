7d33b2c003706ff0bce0783d9d9d4e755a0ddcdd
Sashwat K <sashwat0001@gmail.com>
2019-02-26 15:54:37 +0530

    final stage student calc

--- test-cycle-1/student_calc/calc.sh
+++ test-cycle-1/student_calc/calc.sh
@@ -7,11 +7,59 @@ while read p; do
 	count=$((count+1));
 done < temp
 mean=$((summ/count));
-
+echo "Mean = $mean";
 while read q; do
 	tmp=$((($q-$mean)*($q-$mean)));
 	tmp1=$(($tmp+$tmp1));
 	tmp=0;
 done < temp
 var=$((tmp1/count));
-sd=$(bc <<< "scale=5; sqrt($var)");
+echo "Variance = $var";
+sd=$(bc <<< "scale=5;sqrt($var)");
+echo "Standard deviation = $sd";
+a=$(bc <<< "$mean+(2*$sd)");
+b=$(bc <<< "$mean+$sd");
+c=$(bc <<< "$mean");
+d=$(bc <<< "$mean-($sd)");
+e=$(bc <<< "$mean-(2*$sd)");
+echo "Grade A = $a";
+echo "Grade B = $b";
+echo "Grade C = $c";
+echo "Grade D = $d";
+echo "Grade E = $e";
+
+declare -A grade
+grade[A]=0
+garde[B]=0
+garde[C]=0
+garde[D]=0
+garde[E]=0
+grade[F]=0;
+
+while read r; do
+	if [[ ${r%%.*} -ge ${a%%.*} ]]
+	then
+		grade[A]=`expr ${grade[A]} + 1`
+	elif [[ ${r%%.*} -ge ${b%%.*} ]]
+	then
+		grade[B]=`expr ${grade[B]} + 1`
+	elif [[ ${r%%.*} -ge ${c%%.*} ]]
+	then
+		grade[C]=`expr ${grade[C]} + 1`
+	elif [[ ${r%%.*} -ge ${d%%.*} ]]
+	then
+		grade[D]=`expr ${grade[D]} + 1`
+	elif [[ ${r%%.*} -ge ${e%%.*} ]]
+	then
+		grade[E]=`expr ${grade[E]} + 1`
+	elif [[  ${r%%.*} -le ${e%%.*} ]]
+	then
+		grade[F]=`expr ${grade[F]} + 1`
+	fi
+done < temp
+
+for k in "${!grade[@]}"
+do 
+qw=${grade[$k]}
+echo "$k `printf '*%.0s' {1..$qw}`"
+done
