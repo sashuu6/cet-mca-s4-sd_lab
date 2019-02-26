0ed43ad684496f66b5eba62dc6266f6f2717e8aa
Sashwat K <sashwat0001@gmail.com>
2019-02-26 14:52:50 +0530

    new code on student_calc

--- test-cycle-1/student_calc/calc.sh
+++ test-cycle-1/student_calc/calc.sh
@@ -1,11 +1,17 @@
 count=0;
-awk -F "," 'total_marks= $4+$5+$6+$7+$8+$9+$10 { print total_marks } ' file.csv > temp
+tmp=0;
+tmp1=0;
+awk -F "," 'total_marks= $4+$5+$6+$7+$8+$9+$10 { print total_marks } ' file.csv > temp;
 while read p; do
 	summ=$((summ+$p));
 	count=$((count+1));
 done < temp
 mean=$((summ/count));
 
-while read p; do 
-	
-done
+while read q; do
+	tmp=$((($q-$mean)*($q-$mean)));
+	tmp1=$(($tmp+$tmp1));
+	tmp=0;
+done < temp
+var=$((tmp1/count));
+sd=$(bc <<< "scale=5; sqrt($var)");
