fa63fa876a38cd49678adf3f50b08cf2207d0052
Sashwat K <sashwat0001@gmail.com>
2019-04-04 12:56:35 +0530

    small changes to server.c UDP

--- cycle-2/p2-udp/client.c
+++ cycle-2/p2-udp/client.c
@@ -1,13 +1,13 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h>     
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #define PORT 6565
-#define MAXLINE 1024 
+#define MAXLINE 1024
 
 int main() {
 	int sockfd;
